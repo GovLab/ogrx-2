07a5df2f81fc2fce66c02b053ede9c1ff26f2e5c
mocxd <mark.adkins.hastings@gmail.com>
2016-12-06 12:49:37 -0500

    get filtering papers by category working, and fixed nil errors in plugin

--- source/_plugins/filter_by_field.rb
+++ source/_plugins/filter_by_field.rb
@@ -1,12 +1,18 @@
 module Jekyll
   module FilterDataByField
-    def filter_data_by_field(data, field_name, field_value)  
-      new_rows = []      
-      for row in data
-        field = row[field_name]
-        for value in field 
-          if value.downcase == field_value.downcase
-            new_rows << row
+    def filter_data_by_field(input, field_name, field_value)
+      new_rows = []
+      if !input.nil?
+        for row in input
+          field = row[field_name]
+          if !field.nil?
+            for value in field
+              if !value.nil? && !field_value.nil?
+                if value.downcase == field_value.downcase
+                  new_rows << row
+                end
+              end
+            end
           end
         end
       end
