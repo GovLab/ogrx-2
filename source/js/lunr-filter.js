1298e021b8530a3d37e9b8763287e805be3f2f70
mocxd <mark.adkins.hastings@gmail.com>
2016-12-07 16:54:04 -0500

    basic filter by search working

---
+++ source/js/lunr-filter.js
@@ -0,0 +1,47 @@
+$(function() {
+  var filterLunr = function() {
+    searchTerm = $('#lunr-filter').val();
+
+    var idx = lunr(function () {
+      this.field('id');
+      this.field('title', { boost: 10 });
+      this.field('author');
+      this.field('category');
+      this.field('content');
+    });
+
+    for (var key in window.store) {
+      idx.add({
+        'id': key,
+        'title': window.store[key].title,
+        'author': window.store[key].author,
+        'category': window.store[key].category,
+        'content': window.store[key].content
+      });
+
+      var results = idx.search(searchTerm);
+
+      $( document ).trigger( 'filter:removeClassesContaining', [ 'f-search-' ] );
+
+      for (var r in results) {
+        if (r == 0) {
+          $( document ).trigger( 'filter:add', [ '.f-search-' + results[r].ref ] );
+        } else {
+          $( document ).trigger( 'filter:addOR', [ '.f-search-' + results[r].ref ] );
+        }
+      }
+
+      $( document ).trigger( 'filter:update' );
+    }
+  };
+
+  $('#search-button').click(filterLunr);
+
+  $(window).keydown(function(event){
+    if(event.keyCode == 13) {
+      event.preventDefault();
+      filterLunr();
+    }
+  });
+
+});
