bec2980f1ff9328a103d12ebb48b68d4c2c53132
mocxd <mark.adkins.hastings@gmail.com>
2018-07-31 14:20:02 -0400

    Update 2018-07-31T18:19:35.919Z

--- js/contentful_results.js
+++ js/contentful_results.js
@@ -5,15 +5,17 @@ var contentfulClient = contentful.createClient({
 
 var PAPER_CONTENT_TYPE_ID = 'paper';
 var LIST_CONTENT_TYPE_ID = 'list';
+var BLOG_CONTENT_TYPE_ID = 'blogpost';
 
 var dateFormatOptions = { year: 'numeric', month: 'long', day: 'numeric' }
 
 var slugify = function(text) { return text.toString().toLowerCase().replace(/\s+/g, '-').replace(/[^\w\-]+/g, '').replace(/\-\-+/g, '-').replace(/^-+/, '').replace(/-+$/, ''); }
 var safeslug = function(text) { return text.toString().toLowerCase().replace(/-/g, '%%').replace(/\s+/g, '-'); }
 var deslugify = function(text) { return text.toString().replace(/[\-]/g, ' ').replace('%%', '-'); }
+var getLastInitial = function(text) { return text.toString().replace(/^.*\s+/, '').substring(0, 1); }
+var getLastName = function(text) { return text.toString().replace(/^.*\s+/, ''); }
 
 var results = function (item, basepath) {
-    console.log(basepath);
     if (typeof basepath === "undefined") { basepath = ''; }
     if (item.fields.organization === undefined) { item.fields.organization = []; }
     if (item.fields.objectiveCategory === undefined) { item.fields.objectiveCategory = []; }
@@ -42,31 +44,31 @@ var results = function (item, basepath) {
     "<h3 class='result-item__name'><a href='" + basepath + "paper.html?n=" + encodeURIComponent(safeslug(item.fields.publicationName)).replace('%20', '-') + "' title='" + item.fields.publicationName + "'>" + item.fields.publicationName + "</a></h3>" +
     // "<h3 class='result-item__name'><a href='../paper.html?id=" + item.sys.id + "' title='" + item.fields.publicationName + "'>" + item.fields.publicationName + "</a></h3>" +
     "<p class='result-item__authors'>" +
-    item.fields.authors.map(function(i){ return "<a href='" + basepath + "by/author.html?query=" + i.replace(" ", "+") + "'>" + i + "</a>"}).join(' ') +
+    item.fields.authors.map(function(i){ return "<a href='" + basepath + "by/author.html?name=" + i.replace(" ", "+") + "'>" + i + "</a>"}).join(' ') +
     "</p>" +
     "<div class='result-item__taxonomy result-item__taxonomy--category'>" +
     "<span class='result-item__taxonomy__key'>Category</span>" +
     "<span class='result-item__taxonomy__value'>" +
-    item.fields.innovationCategory.map(function(i){ return "<a href='" + basepath + "all_sdk.html?f=true&category=" + encodeURIComponent(safeslug(i)) + "' class='result-item__tag result-item__tag--" + slugify(i) + "'>" + i + "</a>"}).join(' ') +
+    item.fields.innovationCategory.map(function(i){ return "<a href='" + basepath + "all.html?f=true&category=" + encodeURIComponent(safeslug(i)) + "' class='result-item__tag result-item__tag--" + slugify(i) + "'>" + i + "</a>"}).join(' ') +
     "</span>" +
 
     "</div>" +
     "<div class='result-item__taxonomy result-item__taxonomy--methodology'>" +
     "<span class='result-item__taxonomy__key'>Methodology</span>" +
     "<span class='result-item__taxonomy__value'>" +
-    item.fields.methodology.map(function(i){ return "<a class='result-item__tag' href='" + basepath + "all_sdk.html?f=true&methodology=" + encodeURIComponent(safeslug(i)) + "'>" + i + "</a>"}).join(' ') +
+    item.fields.methodology.map(function(i){ return "<a class='result-item__tag' href='" + basepath + "all.html?f=true&methodology=" + encodeURIComponent(safeslug(i)) + "'>" + i + "</a>"}).join(' ') +
     "</span>" +
     "</div>" +
     "<div class='result-item__taxonomy result-item__taxonomy--objective'>" +
     "<span class='result-item__taxonomy__key'>Objective</span>" +
     "<span class='result-item__taxonomy__value'>" +
-    item.fields.objectiveCategory.map(function(i){ return "<a class='result-item__tag' href='" + basepath + "all_sdk.html?f=true&objective=" + encodeURIComponent(safeslug(i)) + "'>" + i + "</a>"}).join(' ') +
+    item.fields.objectiveCategory.map(function(i){ return "<a class='result-item__tag' href='" + basepath + "all.html?f=true&objective=" + encodeURIComponent(safeslug(i)) + "'>" + i + "</a>"}).join(' ') +
     "</span>" +
     "</div>" +
     "<div class='result-item__taxonomy result-item__taxonomy--type'>" +
     "<span class='result-item__taxonomy__key'>Type</span>" +
     "<span class='result-item__taxonomy__value'>" +
-    item.fields.publicationType.map(function(i){ return "<a class='result-item__tag' href='" + basepath + "all_sdk.html?f=true&type=" + encodeURIComponent(safeslug(i)) + "'>" + i + "</a>"}).join(' ') +
+    item.fields.publicationType.map(function(i){ return "<a class='result-item__tag' href='" + basepath + "all.html?f=true&type=" + encodeURIComponent(safeslug(i)) + "'>" + i + "</a>"}).join(' ') +
     "</span>" +
     "</div>" +
     (item.fields.open ? "" : "<div class='result-item__unpaywall'><p>May be available at</p><a href='http://unpaywall.org/''><img src='../images/unpaywall.png'></a></div>") +
@@ -107,12 +109,18 @@ var singleResult = function(entry) {
 }
 
 var singleList = function(entry) {
-    console.log(entry);
-    console.log (entry.fields.affiliationLogo.fields.file);
-
     return ((entry.fields.articlesList === undefined) ? '' : '<section class="divider"><h1>Selected Readings</h1></section>') +
     entry.fields.articlesList.map(function(i) { return results(i, '../'); }) +
-    '<div class="row"><div class="large-12 column" style="padding: 40px 0"><p style="text-align: center;"><a class="button" href="../selectedreadings.html">Back to Lists</a></p></div></div></div>';
+    '<div class="row"><div class="large-12 column" style="padding: 40px 0"><p style="text-align: center;"><a class="button" href="../selectedreadings.html">All Selected Readings</a></p></div></div></div>';
+}
+
+var singleBlog = function(entry) {
+    return (entry.fields.articlesList === undefined) ? '' : entry.fields.articlesList.map(function(i) { return results(i, '../'); });
+}
+
+var singleAuthor = function(entry) {
+    return '<div class="column small-12 large-4 all-authors-list__list-item">' +
+    '<a href="by/author.html?name=' + encodeURIComponent(entry) + '">'+ entry + '</a></div><div class="alpha_anchor"><a name="' + getLastInitial(entry) + '"></a></div>'
 }
 
 var findGetParameter = function(parameterName) {
@@ -194,21 +202,29 @@ var selectLimitChange = function(e, total) {
     location = location.pathname.split('/').pop() + '?p=' + page + '&limit=' + e.value + paramstring;
 }
 
-var renderEntries = function(el, _category, basepath) {
+var renderEntries = function(el, basepath, _category, _organization, _author) {
     var container = document.getElementById(el);
-    var category;
+    var category, organization, author;
 
     var params = {
         content_type: PAPER_CONTENT_TYPE_ID,
         order: '-sys.createdAt'
     };
 
+    if (typeof basepath === "undefined") {
+        basepath = '';
+    }
     if (typeof _category !== "undefined") {
         category = _category;
         if (category !== null) { params['fields.innovationCategory[match]'] = category; }
     }
-    if (typeof basepath === "undefined") {
-        basepath = '';
+    if (typeof _organization !== "undefined") {
+        organization = _organization;
+        if (organization !== null) { params['fields.organization[match]'] = organization; }
+    }
+    if (typeof _author !== "undefined") {
+        author = _author;
+        if (author !== null) { params['fields.authors[match]'] = author; }
     }
 
     var paramstring = '';
@@ -256,7 +272,7 @@ var renderEntries = function(el, _category, basepath) {
     .then(function(entries) {
         // if skip exceeds total, redirect back to p=1 using current limit to avoid broken page (should only happen if values are manually entered in url)
         if (skip > entries.total) {
-            location = 'all_sdk.html?p=1&limit=' + limit;
+            location = 'all.html?p=1&limit=' + limit;
         }
         container.innerHTML = pagination(page, limit, entries.total, paramstring) + entries.items.map(function(i) { return results(i, basepath); }).join('\n') + pagination(page, limit, entries.total, paramstring);
     })
@@ -312,4 +328,74 @@ var renderList = function(el) {
         }
     })
     .catch(console.error)
+}
+
+var renderBlog = function(el) {
+    var container = document.getElementById(el);
+
+    var name = location.pathname.replace(/^.*\//,'').replace(/\.html$/,'');
+
+    name = deslugify(name);
+    contentfulClient.getEntries({
+        content_type: BLOG_CONTENT_TYPE_ID,
+        'fields.title[match]': name
+    })
+    .then(function(entries) {
+        console.log(name);
+        if (entries.items.length > 0) {
+            console.log(entries.items[0].sys.id);
+            container.innerHTML = singleBlog(entries.items[0]);
+        } else {
+            throw 'No entry found';
+        }
+    })
+    .catch(console.error)
+}
+
+var authorNames = [];
+
+var getAuthors = function(skip, container) {
+    contentfulClient.getEntries({
+        content_type: PAPER_CONTENT_TYPE_ID,
+        select: 'fields.authors',
+        limit: 1000,
+        skip: skip
+    })
+    .then(function(entries) {
+        console.log('Getting authors', skip, '-', skip+1000, 'of', entries.total);
+        if (entries.items.length > 0) {
+            // get all author names
+            for (var i in entries.items) {
+                if (typeof entries.items[i].fields !== 'undefined') {
+                    for (var a in entries.items[i].fields.authors) {
+                        authorNames.push(entries.items[i].fields.authors[a]);
+                    }
+                }
+            }
+        } else {
+            throw 'No entries found';
+        }
+        skip += 1000;
+        if (skip < entries.total) {
+            getAuthors(skip, container);
+        } else {
+            console.log('done');
+
+            // filter for unique names
+            authorNames = Array.from(new Set(authorNames));
+
+            authorNames = authorNames.sort(function(a, b) {
+                return (getLastName(a).toLowerCase() > getLastName(b).toLowerCase()) ? 1 : -1;
+            });
+            console.log(authorNames);
+
+            container.innerHTML = authorNames.map(function(i) { return singleAuthor(i); }).join('\n');
+        }
+    })
+    .catch(console.error)
+}
+
+var renderAuthors = function(el) {
+    var container = document.getElementById(el);
+    getAuthors(0, container);
 }
